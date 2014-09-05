2ef8ba14dcaf31c8880200f391fa9069607c3b22
winhamwr <winhamwr@gmail.com>
2014-09-04 20:52:01 -0400

    Half-finished injection script.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,24 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+        if (document.readyState === "complete") {
+            clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+            var cycleStartColumn = "Implementation",
+                cycleEndColumn = "Done This Week",
+                columnHeaderSelector = ".list-header-name",
+                cardUrlClass = 'a.js-card-name',
+                boardID = '51ae6034ae1428a31800409c',
+                cardID = '5408b0f85e10caee9be0b9d8';
 
-	}
+            console.log("Hello. This message was sent from scripts/inject.js");
+
+            boardUrl = 'https://trell.com/1/boards/' + boardID + '/cards/' + cardID;
+            var cardData = $.getJSON(
+                boardUrl,
+                function(data) {
+                    console.log(data);
+                }
+            );
+        }
 	}, 10);
 });
